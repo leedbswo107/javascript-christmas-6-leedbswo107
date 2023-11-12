2524c1a9a112c638ed9948f2d9cc592b3159965b
leedbswo107 <ljyu1@naver.com>
2023-11-13 00:18:41 +0900

    add: 에러 메시지 추가

--- src/static/Error.js
+++ src/static/Error.js
@@ -1,5 +1,7 @@
 const ERROR = Object.freeze({
-
+    IS_NAN_ERROR : "[ERROR] 숫자가 아닙니다.",
+    DATE_RANGE_ERROR : "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.",
+    MENU_PATTERN_ERROR : "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.",
 });
 // 작성 순서에 따라 하나씩 추가 예정.
 export default ERROR;
