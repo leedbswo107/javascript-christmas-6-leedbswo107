a57f54ebe6e8d33c0830a7c8b6399f1fce350181
leedbswo107 <ljyu1@naver.com>
2023-11-13 22:15:10 +0900

    feat: 에러메시지 변경 및 업데이트

--- src/static/Error.js
+++ src/static/Error.js
@@ -1,7 +1,7 @@
 const ERROR = Object.freeze({
-    IS_NAN_ERROR : "[ERROR] 숫자가 아닙니다.",
-    DATE_RANGE_ERROR : "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.",
-    MENU_PATTERN_ERROR : "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.",
+    DATE_ERROR : "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.",
+    MENU_ERROR : "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.",
+    ONLY_DRINKS_ERROR : "[ERROR] 음료수만 주문 할 수 없습니다."
 });
 // 작성 순서에 따라 하나씩 추가 예정.
 export default ERROR;
