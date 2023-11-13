8c3e41c4ada7f3bf8ec059306b3ebba7a99b6e72
leedbswo107 <ljyu1@naver.com>
2023-11-14 01:46:43 +0900

    feat: 주말 할인 이벤트 추가

--- src/domain/WeekendDiscount.js
+++ src/domain/WeekendDiscount.js
@@ -1,4 +1,20 @@
-class WeekendDiscount {
+import NUMBER from "../static/Number.js";
+import WeekendMaindishCount from "./WeekendMaindishCount.js";
 
+class WeekendDiscount {
+    constructor() {
+        this.weekendMaindishCount = new WeekendMaindishCount();
+    }
+    weekendDiscount(day,maindish) {
+        let weekendDiscountTotal = 0;
+        const weekend = new Object();
+        let maindishCount = this.weekendMaindishCount.maindishCount(maindish);
+        if(NUMBER.weekend.includes(day) === true) {
+            weekendDiscountTotal = (NUMBER.weekendDiscount)*maindishCount;
+            weekend['주말 할인'] = weekendDiscountTotal;
+            return weekend;
+        }
+        return 0;
+    }
 }
 export default WeekendDiscount;
