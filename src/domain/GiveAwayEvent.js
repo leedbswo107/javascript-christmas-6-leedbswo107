cecef33939c46d820e4bec249278c50a5e0ad88c
leedbswo107 <ljyu1@naver.com>
2023-11-14 01:45:44 +0900

    feat: 증정이벤트 추가

--- src/domain/GiveAwayEvent.js
+++ src/domain/GiveAwayEvent.js
@@ -1,4 +1,13 @@
-class GiveAwayEvent {
+import NUMBER from "../static/Number.js";
 
+class GiveAwayEvent {
+    giveAway(totalPrice){
+        let giveAwayChampagne = new Object();
+        if(totalPrice > NUMBER.GIVEAWAYEVENT) {
+            giveAwayChampagne.샴페인 = 1;
+            return giveAwayChampagne; 
+        }
+        return 0;
+    }
 }
 export default GiveAwayEvent;
