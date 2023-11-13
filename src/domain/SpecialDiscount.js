44eea9a665c628734ce78ae798058ba706f36cc0
leedbswo107 <ljyu1@naver.com>
2023-11-14 03:33:44 +0900

    style: 포맷 변경

--- src/domain/SpecialDiscount.js
+++ src/domain/SpecialDiscount.js
@@ -1,4 +1,13 @@
-class SpecialDiscount {
+import NUMBER from "../static/Number.js";
 
+class SpecialDiscount {
+    specialDiscount(day) {
+        const special = new Object();
+        if(NUMBER.special.includes(day) === true) {
+            special['특별 할인'] = Number(NUMBER.specialDiscount);
+            return special;
+        }
+        return 0;
+    }
 }
 export default SpecialDiscount;
