66c129b02fdd28853aa7128d8092208d213d653e
leedbswo107 <ljyu1@naver.com>
2023-11-13 22:12:44 +0900

    feat: 메뉴 입력 검증 기능 추가

--- src/common/Validator.js
+++ src/common/Validator.js
@@ -1,12 +1,44 @@
 import ERROR from "../static/Error.js";
+import NUMBER from "../static/Number.js";
+import PRICE from "../static/Price.js";
+import RULE from "../static/Rule.js";
+
 class Validator {
+    constructor() {}
     validateVisitDate(date) {
-        if(isNaN(date)) throw new Error(`${ERROR.INS_NAN_ERROR}`);
-        if(date < 1 || date > 31) throw new Error(`${ERROR.DATE_RANGE_ERROR}`);
+        if(isNaN(date)) throw new Error(`${ERROR.DATE_ERROR}`);
+        if(date < 1 || date > 31) throw new Error(`${ERROR.DATE_ERROR}`);
     }
     validateMenuPattern(menu) {
-        let pattern =  /^[ㄱ-ㅎ|가-힣]+[-]+\d$/;
-        if(pattern.test(menu) === false) throw new Error(`${ERROR.MENU_PATTERN_ERROR}`);
-     }
+        let pattern =  RULE.menuInputRule;
+        if(pattern.test(menu) === false) throw new Error(`${ERROR.MENU_ERROR}`);
+    }
+    validateCorrectMenu(menu) {
+        if((menu in PRICE.APPETIZER || menu in PRICE.MAIN || menu in PRICE.DESSERT || menu in PRICE.DRINKS) !== true) throw new Error(`${ERROR.MENU_ERROR}`);
+    }
+    validateDuplicateMenu(menu) {
+        const duplicateMenu = [];
+        for(let i = 0; i < menu.length; i++) duplicateMenu.push(menu[i][0]);
+        if(duplicateMenu.length !== new Set(duplicateMenu).size) throw new Error(`${ERROR.MENU_ERROR}`);
+    }
+    validateOrderOnlyDrinks(menu) {
+        const setMenu = new Set(Object.keys(menu));
+        let count = 0;
+        setMenu.forEach(element => {
+            if(element in PRICE.DRINKS) count ++;
+        });
+        if(count === setMenu.size) throw new Error(`${ERROR.ONLY_DRINKS_ERROR}`);
+    }
+    validateOrderAmount(amount) {
+        const menuAmount = Object.values(amount);
+        const count = menuAmount.reduce((total, element) => total + element, 0);
+        if(count > NUMBER.maxAmount) throw new Error("[ERROR] 얼마나 먹는거야 이 돼지 같은놈 !!");
+    }
+    validateMinAmount(amount) {
+        const menuMinAmount = Object.values(amount);
+        menuMinAmount.forEach(element => {
+            if(element <= 0) throw new Error("[ERROR] 최소 1이상이여야 합니다.");
+        });
+    }
 }
 export default Validator;
