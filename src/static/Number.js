2a0f481c1c826a92c23b47eb81d55194138b3005
leedbswo107 <ljyu1@naver.com>
2023-11-14 01:47:19 +0900

    add: 평일,주말,스페셜데이 추가

--- src/static/Number.js
+++ src/static/Number.js
@@ -2,9 +2,12 @@ const NUMBER = {
     eventStart : 1,
     eventEnd : 31,
     eventTarget : 10000,
-
+    샴페인 : 1,
     maxAmount : 20,
-    // minAmount : 
+    
+    weekday : [3,4,5,6,7,10,11,12,13,14,17,18,19,20,21,24,25,26,27,28,31],
+    weekend : [1,2,8,9,15,16,22,23,29,30],
+    special : [3,10,17,24,25,31],
 
     weekdayDiscount : 2023,
     weekendDiscount : 2023,
