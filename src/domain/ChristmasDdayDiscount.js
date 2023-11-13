cf30e2fa3f1aae341b5251961db704aa7a89a511
leedbswo107 <ljyu1@naver.com>
2023-11-14 01:45:33 +0900

    feat: 크리스마스 디데이 할인 추가

--- src/domain/ChristmasDdayDiscount.js
+++ src/domain/ChristmasDdayDiscount.js
@@ -1,4 +1,15 @@
-class ChristmasDdayDiscount {
+import NUMBER from "../static/Number.js";
 
+class ChristmasDdayDiscount {
+    discountAmount(day) {
+        let discountAmount = NUMBER.CHRISTMASDAYEVENT.discount; 
+        const dDay = new Object();
+        if(day >= NUMBER.eventStart && day <= NUMBER.CHRISTMASDAYEVENT.endDate){
+            for(let i = 1; i < day; i++) discountAmount += NUMBER.CHRISTMASDAYEVENT.dayBydayPlus;
+            dDay['크리스마스 디데이 할인'] = discountAmount;
+            return dDay;
+        }
+        return 0;
+    }
 }
 export default ChristmasDdayDiscount;
