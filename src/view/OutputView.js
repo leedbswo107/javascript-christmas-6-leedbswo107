6bdae6b27a6dcbd1fccf822171f01b6c5696b682
leedbswo107 <ljyu1@naver.com>
2023-11-14 17:33:39 +0900

    feat: 불필요 주석 제거 및 사용하지 않는 import 문 제거

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,18 +1,15 @@
 import { Console } from "@woowacourse/mission-utils";
-import PRICE from "../static/Price.js";
 import { OUTPUT_MESSAGE } from "../static/message.js";
+import PRICE from "../static/Price.js";
 
 class OutputView {
-    printIntro() {
-        Console.print(`${OUTPUT_MESSAGE.intro}`);
-    }
     printResultIntro(date) {
         Console.print(`${OUTPUT_MESSAGE.previewFront}${date}${OUTPUT_MESSAGE.previewBack}`);
     }
     printMenu(menu) {
-        Console.print(`${OUTPUT_MESSAGE.orderMenu}`);
+        Console.print(`${(OUTPUT_MESSAGE.orderMenu)}`);
         for (const key in menu) {
-            console.log(`${key} ${menu[key]}${OUTPUT_MESSAGE.quantity}`);
+            Console.print(`${key} ${menu[key]}${OUTPUT_MESSAGE.quantity}`);
         }
     }
     printTotalPriceBeforeDiscount(price) {
@@ -25,18 +22,21 @@ class OutputView {
         else Console.print(`${Object.keys(gift)} ${Object.values(gift)}${OUTPUT_MESSAGE.quantity}`)
     }
     //함수 분리 필요 함
-    printBenefitDetails(dday,weekday,weekend,special) {
+    printBenefitDetails(dday,weekday,weekend,special,giveAway) {
+        //혜택 내역
         Console.print(`${OUTPUT_MESSAGE.benefitDetails}`);
-        if(dday !== 0) Console.print(`${Object.keys(dday)}: -${Object.values(dday).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}${OUTPUT_MESSAGE.won}`);
-        if(weekday !== 0) Console.print(`${Object.keys(weekday)}: -${Object.values(weekday).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}${OUTPUT_MESSAGE.won}`);
-        if(weekend !== 0) Console.print(`${Object.keys(weekend)}: -${Object.values(weekend).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}${OUTPUT_MESSAGE.won}`);
-        if(special !== 0) Console.print(`${Object.keys(special)}: -${Object.values(special).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}${OUTPUT_MESSAGE.won}`);
-        else Console.print(OUTPUT_MESSAGE.none);
+        
+        if(Number(Object.values(dday)) !== 0) Console.print(`${Object.keys(dday)}: -${Object.values(dday).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}${OUTPUT_MESSAGE.won}`);
+        if(Number(Object.values(weekday)) !== 0) Console.print(`${Object.keys(weekday)}: -${Object.values(weekday).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}${OUTPUT_MESSAGE.won}`);
+        if(Number(Object.values(weekend)) !== 0) Console.print(`${Object.keys(weekend)}: -${Object.values(weekend).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}${OUTPUT_MESSAGE.won}`);
+        if(Number(Object.values(special)) !== 0) Console.print(`${Object.keys(special)}: -${Object.values(special).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}${OUTPUT_MESSAGE.won}`);
+        if(Number(Object.values(giveAway)) !== 0) Console.print(`${Object.keys(giveAway)}: -${PRICE.DRINKS.샴페인.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}${OUTPUT_MESSAGE.won}`);
+        if(((Number(Object.values(dday)) === 0) && (Number(Object.values(weekday)) === 0) && (Number(Object.values(weekend)) === 0) && (Number(Object.values(special)) === 0) && (giveAway === 0)) === true) Console.print(OUTPUT_MESSAGE.none);
     }
     //if 절 분리 필요
     printTotalBenefitPrice(total) {
         Console.print(`${OUTPUT_MESSAGE.totalBenefitPrice}`);
-        if(total === 0) Console.print(OUTPUT_MESSAGE.none);
+        if(total === 0) Console.print(`${total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}${OUTPUT_MESSAGE.won}`);
         else Console.print(`-${total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}${OUTPUT_MESSAGE.won}`);
     }
     printTotalPriceAfterDiscount(price) {
