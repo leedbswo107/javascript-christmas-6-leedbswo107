60d9416e05ada72fd19ea6997f9fa952c96e9b9f
leedbswo107 <ljyu1@naver.com>
2023-11-15 00:33:10 +0900

    feat: 1만원 이상 이벤트 참가 조건 추가

--- src/domain/ChristmasDdayDiscount.js
+++ src/domain/ChristmasDdayDiscount.js
@@ -1,10 +1,10 @@
 import NUMBER from "../static/Number.js";
 
 class ChristmasDdayDiscount {
-    discountAmount(day) {
+    discountAmount(day,total) {
         let discountAmount = NUMBER.CHRISTMASDAYEVENT.discount; 
         const dDay = new Object();
-        if(day >= NUMBER.eventStart && day <= NUMBER.CHRISTMASDAYEVENT.endDate){
+        if(total >= NUMBER.eventTarget && (day >= NUMBER.eventStart && day <= NUMBER.CHRISTMASDAYEVENT.endDate)){
             for(let i = 1; i < day; i++) discountAmount += NUMBER.CHRISTMASDAYEVENT.dayBydayPlus;
             dDay['크리스마스 디데이 할인'] = Number(discountAmount);
             return dDay;
--- src/domain/SpecialDiscount.js
+++ src/domain/SpecialDiscount.js
@@ -1,9 +1,9 @@
 import NUMBER from "../static/Number.js";
 
 class SpecialDiscount {
-    specialDiscount(day) {
+    specialDiscount(day,total) {
         const special = new Object();
-        if(NUMBER.special.includes(day) === true) {
+        if((total >= NUMBER.eventTarget) && (NUMBER.special.includes(day) === true)) {
             special['특별 할인'] = Number(NUMBER.specialDiscount);
             return special;
         }
