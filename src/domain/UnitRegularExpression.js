7684fef1ab22939a2a467c82b0e931cf5a9f2915
leedbswo107 <ljyu1@naver.com>
2023-11-14 18:07:54 +0900

    remove: 불필요 코드 제거

--- src/domain/UnitRegularExpression.js
+++
@@ -1,7 +0,0 @@
-class UnitRegularExpression{
-    uniRegularExpression(input) {
-        const commaInput = input.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        return commaInput;
-    }
-}
-export default UnitRegularExpression;
