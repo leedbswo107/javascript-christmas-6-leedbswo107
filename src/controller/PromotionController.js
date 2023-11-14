cc703ca90585c3e797b6c59d705219c6cfec2126
leedbswo107 <ljyu1@naver.com>
2023-11-14 17:27:31 +0900

    refactor: 불필요 코드 삭제

--- src/controller/PromotionController.js
+++ src/controller/PromotionController.js
@@ -48,25 +48,18 @@ class PromotionController {
         this.#afterBenefit = new TotalPriceAfterDiscount();
         this.#badgeFunc = new Badge();
     }
-    
-    intro() {
-        this.#outputView.printIntro();
-        this.#inputDateInput();
-    }
-    async #inputDateInput() {
+    async inputArea() {
         this.#visitDate = await this.#inputView.visitDate();
-        await this.#inputOrderMenu();
-    }
-    async #inputOrderMenu() {
         this.#orderMenu = await this.#inputView.orderMenu();
-        await this.#relatedPrice();
+        
+        await this.relatedPrice();
     }
-    async #relatedPrice() {
-        this.#totalPrice = await this.#totalPriceBeforeDiscount.totalPrice(this.#orderMenu);
+    async relatedPrice() {
+        this.#totalPrice = this.#totalPriceBeforeDiscount.totalPrice(this.#orderMenu);
         this.#commaPrice = await this.#totalPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        await this.#relatedDiscount();
+        this.relatedDiscount();
     }
-    async #relatedDiscount() {
+    relatedDiscount() {
         this.#christmasDiscountAmount = this.#christmasDdayDiscount.discountAmount(this.#visitDate);
         this.#giveAway = this.#giveAwayEvent.giveAway(this.#totalPrice);
         this.#weekdayDiscountTotal = this.#weekdayDiscount.weekdayDiscount(this.#visitDate,this.#orderMenu);
@@ -74,18 +67,21 @@ class PromotionController {
         this.#specialDiscountTotal = this.#specialDiscount.specialDiscount(this.#visitDate);
         this.#totalBenefitPrice = this.#totalBenefit.totalBenefitPrice(this.#christmasDiscountAmount,this.#weekdayDiscountTotal,this.#weekendDiscountTotal,this.#specialDiscountTotal,this.#giveAway);
         this.#totalafterBenefit = this.#afterBenefit.totalPriceAfterDiscount(this.#totalPrice,this.#christmasDiscountAmount,this.#weekdayDiscountTotal,this.#weekendDiscountTotal,this.#specialDiscountTotal);
-        await this.#badgeResult();
+        this.badgeResult();
     }
-    async #badgeResult() {
+    badgeResult() {
         this.#badge = this.#badgeFunc.badge(this.#totalBenefitPrice);
-        this.#displayResult();
+        this.displayResultIntro();
     }
-    #displayResult() {
+    displayResultIntro() {
         this.#outputView.printResultIntro(this.#visitDate);
+        this.displayResult();
+    }
+    displayResult() {
         this.#outputView.printMenu(this.#orderMenu);
         this.#outputView.printTotalPriceBeforeDiscount(this.#commaPrice);
         this.#outputView.printGiveAwayMenu(this.#giveAway);
-        this.#outputView.printBenefitDetails(this.#christmasDiscountAmount,this.#weekdayDiscountTotal,this.#weekendDiscountTotal,this.#specialDiscountTotal);
+        this.#outputView.printBenefitDetails(this.#christmasDiscountAmount,this.#weekdayDiscountTotal,this.#weekendDiscountTotal,this.#specialDiscountTotal,this.#giveAway);
         this.#outputView.printTotalBenefitPrice(this.#totalBenefitPrice);
         this.#outputView.printTotalPriceAfterDiscount(this.#totalafterBenefit);
         this.#outputView.printBadge(this.#badge);
