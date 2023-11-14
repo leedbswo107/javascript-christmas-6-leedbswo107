6af23d5c02a3cce20d3a367c7cb047e810b2ca97
leedbswo107 <ljyu1@naver.com>
2023-11-14 17:32:04 +0900

    feat: 메뉴 에러 메시지 통일

--- src/static/Error.js
+++ src/static/Error.js
@@ -1,7 +1,5 @@
 const ERROR = Object.freeze({
     DATE_ERROR : "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.",
     MENU_ERROR : "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.",
-    ONLY_DRINKS_ERROR : "[ERROR] 음료수만 주문 할 수 없습니다."
 });
-// 작성 순서에 따라 하나씩 추가 예정.
 export default ERROR;
